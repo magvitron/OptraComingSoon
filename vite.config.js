b63660e2d3169f0cd46f2a0553bdab23ee240cec
magvitron <magvitron@gmail.com>
2022-06-07 23:48:00 +0200

    done

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'OpraSolutions',
+  base:'OptraComingSoon',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1600,
